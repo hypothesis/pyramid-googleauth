064f2004642cb9467bb0bdc107f892a7e3ca9e15
Marcos Prieto <marcospri@gmail.com>
2021-04-20 10:25:17 +0200

    h_pyramid_google_oauth services and views

---
+++ src/h_pyramid_google_oauth/routes.py
@@ -0,0 +1,31 @@
+"""The routes for the Pyramid app."""
+
+
+def add_routes(config):
+    """Register all routes."""
+
+    config.add_route(
+        "h_pyramid_google_oauth_login",
+        config.registry.settings.get(
+            "h_pyramid_google_oauth.login_route", "/ui/api/login"
+        ),
+    )
+    config.add_route(
+        "h_pyramid_google_oauth_login_callback",
+        config.registry.settings.get(
+            "h_pyramid_google_oauth.login_callback_route", "/ui/api/login_callback"
+        ),
+    )
+    config.add_route(
+        "h_pyramid_google_oauth_logout",
+        config.registry.settings.get(
+            "h_pyramid_google_oauth.logout_route", "/ui/api/logout"
+        ),
+    )
+
+    config.add_route(
+        "h_pyramid_google_oauth_login_failure",
+        config.registry.settings.get(
+            "h_pyramid_google_oauth.login_failure_route", "/ui/api/login_failure"
+        ),
+    )
---
+++ src/h_pyramid_google_oauth/security.py
@@ -0,0 +1,51 @@
+from typing import List, NamedTuple
+
+from pyramid.authentication import SessionAuthenticationHelper
+from pyramid.security import Allowed, Denied
+
+DEFAULT_PERMISSION = "admin"
+
+
+class Identity(NamedTuple):
+    userid: str
+    permissions: List[str]
+
+
+def _get_identity(userid):
+    if userid and userid.endswith("@hypothes.is"):
+        return Identity(userid, permissions=[DEFAULT_PERMISSION])
+
+    return Identity("", [])
+
+
+class GoogleSecurityPolicy:
+    def __init__(self):
+        self._session_authentication_helper = SessionAuthenticationHelper()
+
+    def identity(self, request):
+        userid = self.authenticated_userid(request)
+
+        get_identity = request.registry.settings.get(
+            "h_pyramid_google_oauth.get_identity_callable", _get_identity
+        )
+
+        return get_identity(userid)
+
+    def authenticated_userid(self, request):
+        return self._session_authentication_helper.authenticated_userid(request)
+
+    def permits(self, request, context, permission):
+        return _permits(self, request, context, permission)
+
+    def remember(self, request, userid, **kwargs):
+        return self._session_authentication_helper.remember(request, userid, **kwargs)
+
+    def forget(self, request, **kwargs):
+        return self._session_authentication_helper.forget(request, **kwargs)
+
+
+def _permits(policy, request, _context, permission):
+    if permission in policy.identity(request).permissions:
+        return Allowed("allowed")
+
+    return Denied("denied")
