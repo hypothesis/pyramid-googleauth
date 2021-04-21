2cd0e3cf9659b6db6b0a9cd3445e82a2c8b08d22
Marcos Prieto <marcospri@gmail.com>
2021-04-21 15:53:37 +0200

    Allow clients to bring their own SessionHelper
    Default to SessionAuthenticationHelper with a prefix

--- tests/unit/security_test.py
+++ tests/unit/security_test.py
@@ -5,6 +5,9 @@ from pyramid.security import Allowed, Denied
 
 
 class TestGoogleSecurityPolicy:
+    def test_session(self, policy):
+        assert policy.session().userid_key == "googleauth.userid"
+
     @pytest.mark.parametrize(
         "userid,expected_identity",
         [
@@ -13,7 +16,7 @@ class TestGoogleSecurityPolicy:
         ],
     )
     def test_identity(self, policy, pyramid_request, userid, expected_identity):
-        pyramid_request.session["auth.userid"] = userid
+        pyramid_request.session["googleauth.userid"] = userid
 
         assert policy.identity(pyramid_request) == expected_identity
 
@@ -21,7 +24,7 @@ class TestGoogleSecurityPolicy:
         assert policy.identity(pyramid_request) == []
 
     def test_authenticated_userid(self, policy, pyramid_request):
-        pyramid_request.session["auth.userid"] = "testuser@hypothes.is"
+        pyramid_request.session["googleauth.userid"] = "testuser@hypothes.is"
 
         assert policy.authenticated_userid(pyramid_request) == "testuser@hypothes.is"
 
@@ -33,7 +36,7 @@ class TestGoogleSecurityPolicy:
         ],
     )
     def test_permits(self, policy, pyramid_request, permission, expected_result):
-        pyramid_request.session["auth.userid"] = "testuser@hypothes.is"
+        pyramid_request.session["googleauth.userid"] = "testuser@hypothes.is"
 
         assert (
             policy.permits(pyramid_request, sentinel.context, permission)
