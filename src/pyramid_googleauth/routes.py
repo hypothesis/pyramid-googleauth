8f46328ae52453511e983da62076ee7a3eb5b77c
Marcos Prieto <marcos.prieto@hypothes.is>
2021-04-21 13:10:53 +0200

    Better defaults for route locations
    Co-authored-by: Sean Hammond <seanh@users.noreply.github.com>

--- src/pyramid_googleauth/routes.py
+++ src/pyramid_googleauth/routes.py
@@ -6,25 +6,25 @@ def add_routes(config):
 
     config.add_route(
         "pyramid_googleauth_login",
-        config.registry.settings.get("pyramid_googleauth.login_route", "/ui/api/login"),
+        config.registry.settings.get("pyramid_googleauth.login_route", "/googleauth/login"),
     )
     config.add_route(
         "pyramid_googleauth_login_callback",
         config.registry.settings.get(
-            "pyramid_googleauth.login_callback_route", "/ui/api/login_callback"
+            "pyramid_googleauth.login_callback_route", "/googleauth/login/callback"
         ),
     )
     config.add_route(
         "pyramid_googleauth_logout",
         config.registry.settings.get(
-            "pyramid_googleauth.logout_route", "/ui/api/logout"
+            "pyramid_googleauth.logout_route", "/googleauth/logout"
         ),
     )
 
     config.add_route(
         "pyramid_googleauth_login_failure",
         config.registry.settings.get(
-            "pyramid_googleauth.login_failure_route", "/ui/api/login_failure"
+            "pyramid_googleauth.login_failure_route", "/googleauth/login/failure"
         ),
     )
     config.scan()
